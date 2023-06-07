be3087bc88860a8f6b81014eeece589512a10f79
Laurent COLLET <laurentpro.collet@gmail.com>
2023-06-07 09:33:34 +0200

    fixed reactive display on header

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,31 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, DoCheck} from '@angular/core';
+import { Router} from "@angular/router";
 import {RoutesPathEnum} from "../../enums/routes-path.enum";
-import {Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
-  currentPath$! :Observable<string>;
-  constructor(private router : Router, private route : ActivatedRoute) {
+export class HeaderComponent implements DoCheck{
+  currentPath! : string;
+  constructor(private router : Router) {
   }
-  ngOnInit() {
-   this.route.params.subscribe( value => console.log(value));
-  }
-
-  isLandingPage() : boolean {
-    console.log(this.currentPath$)
-    return true;
-  }
-  goToHomePage() {
-    this.router.navigateByUrl(RoutesPathEnum.Home);
+  ngDoCheck() {
+    this.currentPath = this.router.url;
+    console.log("do check :", this.currentPath)
   }
 
-  goToCreateSessionPage() {
-    this.router.navigateByUrl(RoutesPathEnum.CreateSession);
+  goToHomePage() : Promise<boolean> {
+     return this.router.navigateByUrl(RoutesPathEnum.Home);
+  }
+
+  goToCreateSessionPage() : Promise<boolean> {
+    return this.router.navigateByUrl(RoutesPathEnum.CreateSession);
   }
 
 }
