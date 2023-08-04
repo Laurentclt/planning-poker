3630b350b19369e74e982b36ddc47e79c2ed9d94
Laurent COLLET <laurentpro.collet@gmail.com>
2023-08-04 16:21:53 +0200

    add placement for players

--- src/app/modules/game/components/game-session/game-session.component.ts
+++ src/app/modules/game/components/game-session/game-session.component.ts
@@ -1,11 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Card} from "../../../../shared/models/card.model";
-import {GameService} from "../../services/game.service";
 import {Player} from "../../../players/models/player.model";
 import {RoundState} from "../../enums/round-state.enum";
 import {StateService} from "../../../../core/services/state.service";
 import {Router} from "@angular/router";
-import {Modal} from "../../../../shared/models/modal.model";
 import {DatabaseService} from "../../../../core/services/database.service";
 import {Observable} from "rxjs";
 
@@ -17,9 +15,14 @@ import {Observable} from "rxjs";
 export class GameSessionComponent implements OnInit {
     votingCards: Card[] = [];
     players$! : Observable<Player[]>
+    playersTop : Player[] = []
+    playersLeft : Player[] = []
+    playersRight : Player[] = []
+    playersBottom : Player[] = []
     message!: string;
     roundState!: RoundState;
     protected readonly RoundState = RoundState;
+
 
     constructor(private router: Router, private databaseService: DatabaseService, public stateService: StateService) {
     }
@@ -37,6 +40,19 @@ export class GameSessionComponent implements OnInit {
             .catch(err => console.log(err))
         this.roundState = RoundState.UserDidNotVote;
         this.players$ = this.databaseService.getActivePlayers$(this.router.url) as Observable<Player[]>
+        this.players$.subscribe(data => data.filter((value, index, array) => {
+            if (index === 1 || index === 5 || index === 7 || index === 9) {
+                this.playersTop.push(array[index])
+            }else if (index === 2 || index === 10 ) {
+                this.playersLeft.push(array[index])
+            } else if (index === 0 || index === 4 || index === 6 || index === 8) {
+                this.playersBottom.push(array[index])
+            } else if (index === 3 || index === 11) {
+                this.playersRight.push(array[index])
+            }
+
+
+        }  ))
     }
 
 
